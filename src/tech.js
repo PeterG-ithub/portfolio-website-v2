5816bf473b9df19b50769f5188a2d2b75b4ee858
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2025-02-06 12:21:28 -0800

    feat: Add more technologies
    - Add more logoes for the new technologies
    - Add new size to object
    - Replace img size size from css to object

--- src/tech.js
+++ src/tech.js
@@ -4,54 +4,60 @@
 const techStack = [
     {
         name: "Git",
-        icon: "/icons/git.svg", // Path to the icon
+        icon: "/icons/git.svg",
         background: "#45251e",
         description: "Version control system",
+        size: "32px",  // You can add the size here if you want
     },
     {
         name: "Tailwind",
         background: "#123b4e",
         icon: "/icons/tailwind.svg",
         description: "CSS framework",
+        size: "32px",  // Add size if needed
     },
     {
         name: "JavaScript",
         icon: "/icons/javascript.svg",
         background: "#4a4308",
-        description: "Programming language for the web",
+        description: "Programming language",
+        size: "32px",  // Add size if needed
     },
     {
         name: "C++",
         icon: "/icons/cpp.png",
         background: "#041d40",
         description: "Programming language",
+        size: "32px",  // Add size if needed
     },
     {
-        name: "WIP",
-        icon: "/icons/wip.png",
-        background: "#002e6d",
-        description: "Work in Progress",
+        name: "Python",
+        icon: "/icons/python.svg",
+        size: "60px",  // Already included here
+        background: "#c5ebe4",
+        description: "Programming language",
     },
     {
-        name: "WIP",
-        icon: "/icons/wip.png",
-        background: "#002e6d",
+        name: "PostgreSQL",
+        icon: "/icons/psql.svg",
+        background: "#99b3c8",
         description: "Work in Progress",
+        size: "36px",  // Add size if needed
     },
     {
-        name: "WIP",
-        icon: "/icons/wip.png",
-        background: "#002e6d",
+        name: "Ruby on Rails",
+        icon: "/icons/ruby-on-rails.svg",
+        background: "#eb9999",
         description: "Work in Progress",
+        size: "36px",  // Add size if needed
     },
     {
-        name: "WIP",
-        icon: "/icons/wip.png",
-        background: "#002e6d",
+        name: "Vite",
+        icon: "/icons/vite.svg",
+        background: "#1c1733",
         description: "Work in Progress",
+        size: "32px",  // Add size if needed
     },
-    
-    // Add more techs here
 ];
 
 // Function to create a tech card
@@ -61,7 +67,7 @@ function createTechCard(tech) {
 
     card.innerHTML = `
         <div class="img-container" style="background-color: ${tech.background};">
-            <img src="${tech.icon}" alt="${tech.name}">
+            <img src="${tech.icon}" alt="${tech.name}" style="width: ${tech.size}; height: ${tech.size};">
         </div>
         <div>
             <h4>${tech.name}</h4>
